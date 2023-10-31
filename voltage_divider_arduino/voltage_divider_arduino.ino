48948772f2ab644d4bb90af0280539d9ea2a96b0
gyeongseo0504 <125629732+gyeongseo0504@users.noreply.github.com>
2023-11-01 00:07:03 +0900

    Add files via upload

--- voltage_divider_arduino/voltage_divider_arduino.ino
+++ voltage_divider_arduino/voltage_divider_arduino.ino
@@ -1,4 +1,4 @@
-void setup() 
+void setup()
 {
   Serial.begin(115200);
 }
@@ -6,6 +6,19 @@ void setup()
 void loop()
 {
   int sensorValue = analogRead(A0);
-  float voltage = sensorValue * (5.0 / 1023.0);
-  Serial.println(voltage);
+  float Vout = sensorValue * (5.0 / 1023.0);
+  float R1 = 15.0;
+  float R2 = (Vout * R1) / (5.0 - Vout);
+
+  // Vout
+  Serial.print("출력 전압 (Vout): ");
+  Serial.print(Vout, 2); // 소수점 둘째 자리까지 출력
+  Serial.println(" V");
+
+  // R2값
+  Serial.print("두 번째 저항 R2 값: ");
+  Serial.print(R2, 2); // 소수점 둘째 자리까지 출력
+  Serial.println(" 옴");
+
+  delay(1000);
 }
